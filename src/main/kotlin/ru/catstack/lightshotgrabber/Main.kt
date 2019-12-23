a2272263c6a5abdf15cf47bd64955255a04ae933
Gorr <gorrgorrob@gmail.com>
2019-12-23 14:30:54 +0500

    Image counts & version system

--- src/main/kotlin/ru/catstack/lightshotgrabber/Main.kt
+++ src/main/kotlin/ru/catstack/lightshotgrabber/Main.kt
@@ -6,7 +6,7 @@ import java.io.File
 import java.util.*
 
 fun main() {
-    println("LightShot Grabber v1.1 by CatStack (2019)")
+    println("LightShot Grabber $VERSION by CatStack (2019)")
     println("When you upload image to prnt.sc, you will get URL like this: https://prnt.sc/xxxxxx")
     println("Enter Image ID (xxxxxx) here:")
     val folder = File("imagesOut/")
@@ -22,13 +22,13 @@ fun main() {
     val random = Random()
 
     for (i in 1..99999) {
-        print("(i) $imageID - ")
+        print("($i) $imageID - ")
 
         val imageURL = imageParser.parseImageUrl(imageID.toString())
         imageDownloader.downloadImage(imageURL, imageID.toString())
         println("successful")
 
         imageID.inc()
-        Thread.sleep(1000 + random.nextInt(500).toLong())
+        Thread.sleep(100 + random.nextInt(400).toLong())
     }
 }
